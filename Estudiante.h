cf043cde61a0ff8f784d73e3d12064e9ccb7c110
LCrespoG <lgarccre@myuax.com>
2024-10-14 22:19:00 +0200

    Ejercicio 1

---
+++ Estudiante.h
@@ -0,0 +1,20 @@
+//
+// Created by LCGpe on 14/10/2024.
+//
+
+#ifndef ESTUDIANTE_H
+#define ESTUDIANTE_H
+
+#include <string>
+#include "Persona.h"
+
+class Estudiante : public Persona {
+    std::string grado;
+
+public:
+    void setGrado(const std::string &grado);
+    std::string getGrado();
+    void mostrarDetalles();
+};
+
+#endif
---
+++ Persona.h
@@ -0,0 +1,26 @@
+//
+// Created by LCGpe on 14/10/2024.
+//
+
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <string>
+
+class Persona {
+    std::string nombre;
+    int edad;
+    std::string genero;
+
+public:
+    void setNombre(const std::string &nombre);
+    std::string getNombre();
+    void setEdad(int edad);
+    int getEdad();
+    void setGenero(const std::string &genero);
+    std::string getGenero();
+};
+
+
+
+#endif //PERSONA_H
